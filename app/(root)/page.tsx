8608fb1f9c5f51e617a5447b3312d56aafece4e9
Ivan-6332 <ivanakalanka@gmail.com>
2025-01-29 03:43:24 +0530

    sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/ui/HeaderBox'
+import RightSidebar from '@/components/ui/RightSidebar';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Ivan' };
+  const loggedIn = { firstName: 'Ivan', lastName: 'De Zoysa', email: 'ivanakalanka@gmail.com'};
 
   return (
     <section className="home">
@@ -22,7 +23,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS 
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50}, { currentBalance: 600.50}]}
+      />
     </section>
   )
 }
