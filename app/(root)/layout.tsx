0b862dcd586ed0e7d6c3be64aa5647d8588a2dd4
Ivan-6332 <ivanakalanka@gmail.com>
2025-01-28 05:57:52 +0530

    Home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Ivan', lastName: 'De Zoysa'}
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
