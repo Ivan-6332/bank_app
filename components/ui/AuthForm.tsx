ea54b761e6fa89fdc4c1ee187778b336e8ad6ed9
Ivan-6332 <ivanakalanka@gmail.com>
2025-02-01 14:35:40 +0530

    signin and logging and logout

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -49,22 +49,23 @@ const AuthForm = ({ type }: { type: string}) => {
 
         if(type === 'sign-up'){
             const newUser = await signUp(data);
+
             setUser(newUser);
         }
  
         if(type === 'sign-in'){
             const response = await signIn({
-                email:data.email,
-                password: data.password,
-            })
+                email: data.email,
+                password: data.password
+            });
 
-            if(response) router.push('/')
+            if (response) router.push('/');
         }
     } catch (error) {
-        console.log(error)
+        console.log(error);
     } finally {
         setIsLoading(false);
-    } 
+    }
   }
     
   return (
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,46 @@
 'use server';
 
-import { createSessionClient } from '../appwrite';
+import { ID } from 'node-appwrite';
+import { createAdminClient, createSessionClient } from '../appwrite';
+import { cookies } from 'next/headers';
+import { parseStringify } from '../utils';
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
     try{
-        // Mutation / Database / Make fetch
+        const {account} = await createAdminClient();
+
+        const response = await account.createEmailPasswordSession(email, password);
+
+        return parseStringify(response);
+
     } catch (error) {
         console.error('Error', error);
     }
 }
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+    if (!email || !password || !firstName || !lastName) {
+        throw new Error('Missing required user data');
+    }
     try{
-        // create a user account
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+        );
+        const session = await account.createEmailPasswordSession(email, password);
+
+        (await cookies()).set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify(newUserAccount);
         console.log('User data:', userData);
     } catch (error) {
         console.error('Error', error);
@@ -21,10 +50,23 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
     try {
       const { account } = await createSessionClient();
-      return await account.get();
+
+      const user = await account.get();
+      return parseStringify(user);
+      
     } catch (error) {
       console.error('Error', error);
       return null;
     }
   }
-  
+
+  export const logoutAccount = async () => {
+    try{
+        const { account } = await createSessionClient();
+        (await cookies()).delete("appwrite-session");
+        await account.deleteSession("current");
+    } catch (error) {
+        console.error('Error', error);
+        return null;
+    }
+}
