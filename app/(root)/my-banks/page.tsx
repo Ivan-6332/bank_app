9a0c0105d875262c5d98f1f3f2f052b05e0aa837
Ivan-6332 <ivanakalanka@gmail.com>
2025-02-04 09:45:17 +0530

    new pages for, My banks, Payment-Tranfer-history and transaction-history

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const MyBanks = () => {
   return (
-    <div>MyBanks</div>
-  )
-}
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh', 
+      textAlign: 'center', 
+      background: 'linear-gradient(to right,rgb(231, 238, 251),rgb(244, 252, 255))', 
+      color: '#333',
+      padding: '20px'
+    }}>
+      <h1 style={{ fontSize: '2.8rem' }}>🚧 Oops! This page is under construction 🏗️</h1>
+      <p style={{ fontSize: '1.8rem' }}>We&apos;re working hard to bring this to you soon! ⏳✨</p>
+      <p style={{ fontSize: '1.8rem' }}>Stay tuned! 🚀💖</p>
+    </div>
+  );
+};
 
-export default MyBanks
+export default MyBanks;
--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const TransactionHistory = () => {
   return (
-    <div>TransactionHistory</div>
-  )
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh', 
+      textAlign: 'center', 
+      background: 'linear-gradient(to right,rgb(231, 238, 251),rgb(244, 252, 255))', 
+      color: '#333',
+      padding: '20px'
+    }}>
+      <h1 style={{ fontSize: '2.8rem' }}>🚧 Oops! This page is under construction 🏗️</h1>
+      <p style={{ fontSize: '1.8rem' }}>We&apos;re working hard to bring this to you soon! ⏳✨</p>
+      <p style={{ fontSize: '1.8rem' }}>Stay tuned! 🚀💖</p>
+    </div>
+  );
 }
 
 export default TransactionHistory
