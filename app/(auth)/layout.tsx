464571bec02136fd91c2180d2f56b72a1d9adea9
Ivan-6332 <ivanakalanka@gmail.com>
2025-02-04 10:13:17 +0530

    layout update (Sign-In and Sign-Up)

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,13 @@
-import Image from "next/image";
-
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <main className="flex min-h-screen w-full justify-between font-inter">
-          {children}
-          <div className="auth-asset">
-            <div>
-              <Image 
-                src="/icons/auth-image.svg"
-                alt="Auth Image"
-                width={500}
-                height={500}
-              />
-            </div>
-          </div>
-      </main>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex min-h-screen w-full justify-center items-center bg-gradient-to-br from-[#d9f1ff] to-[#a1c4fd] font-inter">
+      <div className="w-full max-w-5xl p-8 bg-white/70 backdrop-blur-lg shadow-xl rounded-2xl">
+        {children}
+      </div>
+    </main>
+  );
+}
